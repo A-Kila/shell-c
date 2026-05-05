dfc903cb149246898cc479ff64680d1da9d5b754
Andria Kilasonia <akila19@freeuni.edu.ge>
2026-05-05 16:08:33 +0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     // Flush after every printf
@@ -10,6 +11,10 @@ int main(int argc, char *argv[]) {
 
         char command[256];
         scanf("%255s", command);
+
+        if (strcmp(command, "exit")) {
+            break;
+        }
 
         // error message
         printf("%s: command not found\n", command);
