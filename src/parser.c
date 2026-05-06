4fd4fc61c86bdbb46a3c4cfe7bf8ee430a4c5b2e
Andria Kilasonia <akila19@freeuni.edu.ge>
2026-05-07 03:56:54 +0400

    codecrafters submit [skip ci]

--- src/parser.c
+++ src/parser.c
@@ -1,12 +1,11 @@
 #include "parser.h"
 #include <string.h>
-#include <sys/syslimits.h>
 
 bool parse(command_t *command_out, char *input) {
     command_out->argc = 0;
 
     for (char *token = strtok(input, " "); token != NULL; token = strtok(NULL, " ")) {
-        if (command_out->argc >= MAX_INPUT - 1) break;
+        if (command_out->argc >= MAX_ARGS - 1) break;
         command_out->argv[command_out->argc++] = token;
     }
 
