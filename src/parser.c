15d5f443b500cb14ddd5483b5ea34f980a9006dd
Andria Kilasonia <akila19@freeuni.edu.ge>
2026-05-20 18:12:41 +0400

    codecrafters submit [skip ci]

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,8 @@
 #include "parser.h"
 #include "shell.h"
+#include <_string.h>
+#include <_strings.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,29 +11,61 @@ static char *replace_home(const char *token) {
     const char *home = getenv("HOME");
     if (!home) return NULL;
 
-    size_t slen = strlen(home) + strlen(token); // no + 1 neccessry, token has extra ~
-    char *replaced = malloc(slen);
-    snprintf(replaced, slen, "%s%s", home, token + 1);
+    size_t len = strlen(home) + strlen(token); // no + 1 neccessry, token has extra ~
+    char *replaced = malloc(len);
+    snprintf(replaced, len, "%s%s", home, token + 1);
 
     return replaced;
+}
+
+static char *strcat_heap(const char *str1, const char *str2, bool is_space) {
+    char *res = malloc(strlen(str1) + strlen(str2) + (is_space ? 2 : 1));
+    strcat(res, str1);
+    if (is_space) strcat(res, " ");
+    strcat(res, str2);
+
+    return res;
+}
+
+static char *handle_sqoutes(const char *token, char **rest) {
+    if (!*rest) *rest = "";
+
+    char *full_string = strcat_heap(token, *rest, true);
+
+    char *res = NULL;
+    size_t res_len = 0;
+    for (token = strtok_r(full_string, "'", rest); token; token = strtok_r(*rest, "'", rest)) {
+        if (!res) {
+            res = strdup(token);
+            continue;
+        }
+        char *tmp = res;
+        res = strcat_heap(res, token, false);
+        free(tmp);
+
+        if (*rest && *rest[0] == ' ') break;
+    }
+
+    return res;
 }
 
 bool parse(command_t *command_out, char *input) {
     command_out->argc = 0;
 
-    for (char *token = strtok(input, " "); token != NULL; token = strtok(NULL, " ")) {
+    char *rest;
+    for (char *token = strtok_r(input, " ", &rest); token != NULL;) {
         if (command_out->argc >= MAX_ARGS - 1) break;
+ 
+        char *argv = NULL;
 
-        char *argv = strdup(token);
-        if (token[0] == '~') {
-            char *tmp = replace_home(token);
-            if (tmp) {
-                free(argv);
-                argv = tmp;
-            }
-        }
+        if (index(token, '\'')) argv = handle_sqoutes(token, &rest);
+        if (token[0] == '~') argv = replace_home(token);
 
+        if (!argv) argv = strdup(token);
         command_out->argv[command_out->argc++] = argv;
+ 
+        // printf("argv - %s;  rest - %s\n", argv, rest);
+        token = strtok_r(rest, " ", &rest);
     }
 
     command_out->argv[command_out->argc] = NULL;
