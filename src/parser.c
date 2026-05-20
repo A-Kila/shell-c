af440c03a398353017ae454f0f8747ec48ac8558
Andria Kilasonia <akila19@freeuni.edu.ge>
2026-05-20 18:52:22 +0400

    codecrafters submit [skip ci]

--- src/parser.c
+++ src/parser.c
@@ -6,73 +6,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-static char *replace_home(const char *token) {
+static char *apply_tilde(const char *token) {
     const char *home = getenv("HOME");
     if (!home) return NULL;
 
-    size_t len = strlen(home) + strlen(token); // no + 1 neccessry, token has extra ~
+    size_t len = strlen(home) + strlen(token); // no +1 necessary, token has extra ~
     char *replaced = malloc(len);
     snprintf(replaced, len, "%s%s", home, token + 1);
 
     return replaced;
 }
 
-static char *handle_sqoutes(const char *token, char **rest) {
-    char buf[BUFFER_SIZE] = {0};
-    size_t len = 0;
-    bool inside = false;
-
-    for (const char *ch = token; *ch; ch++) {
-        if (*ch == '\'') {
-            inside = !inside; 
-            continue; 
-        }
-
-        buf[len++] = *ch;
-    }
-
-    if (inside && *rest) {
-        buf[len++] = ' ';
-        for (char *ch = *rest; true; ch++) {
-            if (*ch == '\0') { 
-                *rest = NULL; 
-                break; 
-            }
- 
-            if (*ch == '\'') { 
-                inside = !inside; 
-                continue; 
-            }
-
-            if (!inside && *ch == ' ') { 
-                *rest = ch + 1; 
-                break; 
-            }
-
-            buf[len++] = *ch;
-        }
-    }
-
-    return strdup(buf);
-}
-
 bool parse(command_t *command_out, char *input) {
     command_out->argc = 0;
 
-    char *rest;
-    for (char *token = strtok_r(input, " ", &rest); token != NULL;) {
-        if (command_out->argc >= MAX_ARGS - 1) break;
- 
-        char *argv = NULL;
+    char buf[BUFFER_SIZE];
+    size_t len = 0;
+    bool inside_sq = false;
 
-        if (index(token, '\'')) argv = handle_sqoutes(token, &rest);
-        else if (token[0] == '~') argv = replace_home(token);
-
-        if (!argv) argv = strdup(token);
-        command_out->argv[command_out->argc++] = argv;
- 
-        // printf("argv - %s;  rest - %s\n", argv, rest);
-        token = strtok_r(rest, " ", &rest);
+    for (char *p = input; ; p++) {
+        if (*p == '\'') {
+            inside_sq = !inside_sq;
+        } else if (*p == '\0' || (*p == ' ' && !inside_sq)) {
+            if (len > 0) {
+                buf[len] = '\0';
+                char *arg = (buf[0] == '~') ? apply_tilde(buf) : NULL;
+                if (!arg) arg = strdup(buf);
+                command_out->argv[command_out->argc++] = arg;
+                len = 0;
+            }
+            if (*p == '\0') break;
+        } else {
+            buf[len++] = *p;
+        }
     }
 
     command_out->argv[command_out->argc] = NULL;
