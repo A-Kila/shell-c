586354ba519605eb6214eafd1f881266b2dc1194
Andria Kilasonia <akila19@freeuni.edu.ge>
2026-05-20 18:57:30 +0400

    codecrafters submit [skip ci]

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-static char *apply_tilde(const char *token) {
+static char *replace_head(const char *token) {
     const char *home = getenv("HOME");
     if (!home) return NULL;
 
@@ -24,21 +24,29 @@ bool parse(command_t *command_out, char *input) {
     size_t len = 0;
     bool inside_sq = false;
 
-    for (char *p = input; ; p++) {
-        if (*p == '\'') {
+    for (char *ch = input; ; ch++) {
+        if (*ch == '\'') {
             inside_sq = !inside_sq;
-        } else if (*p == '\0' || (*p == ' ' && !inside_sq)) {
+            continue;
+        }
+
+        if (*ch == '\0' || (*ch == ' ' && !inside_sq)) {
             if (len > 0) {
-                buf[len] = '\0';
-                char *arg = (buf[0] == '~') ? apply_tilde(buf) : NULL;
+                char *arg = (buf[0] == '~') ? replace_head(buf) : NULL;
+
                 if (!arg) arg = strdup(buf);
                 command_out->argv[command_out->argc++] = arg;
+ 
+                buf[len] = '\0';
                 len = 0;
             }
-            if (*p == '\0') break;
-        } else {
-            buf[len++] = *p;
+
+            if (*ch == '\0') break;
+
+            continue;
         }
+ 
+        buf[len++] = *ch;
     }
 
     command_out->argv[command_out->argc] = NULL;
