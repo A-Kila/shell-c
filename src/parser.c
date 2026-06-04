e3235e089055ce1dcfd5cb1f6fde45433c8070cb
Andria Kilasonia <akila19@freeuni.edu.ge>
2026-06-05 00:16:20 +0400

    codecrafters submit [skip ci]

--- src/parser.c
+++ src/parser.c
@@ -6,9 +6,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NULL_CHAR '\0'
+#define SPACE ' '
+#define TILDE '~'
+#define SINGLE_QUOTE '\''
+#define DOUBLE_QUOTE '"'
+#define ESCAPE '\\'
+#define DOLLAR '$'
+
 static char *replace_head(const char *token) {
     const char *home = getenv("HOME");
-    if (!home) return NULL;
+    if (!home || token[0] != TILDE) return NULL;
 
     size_t len = strlen(home) + strlen(token); // no +1 necessary, token has extra ~
     char *replaced = malloc(len);
@@ -22,18 +30,23 @@ bool parse(command_t *command_out, char *input) {
 
     char buf[BUFFER_SIZE];
     size_t len = 0;
-    bool inside_sq = false;
+    bool inside_sq, inside_dq = false;
 
     for (char *ch = input; ; ch++) {
-        if (*ch == '\'') {
+        if (!inside_dq && *ch == SINGLE_QUOTE) {
             inside_sq = !inside_sq;
             continue;
         }
 
-        if (*ch == '\0' || (*ch == ' ' && !inside_sq)) {
+        if (!inside_sq && *ch == DOUBLE_QUOTE) {
+            inside_dq = !inside_dq;
+            continue;
+        }
+
+        if (*ch == NULL_CHAR || (*ch == SPACE && !inside_sq && !inside_dq)) {
             if (len > 0) {
-                buf[len] = '\0';
-                char *arg = (buf[0] == '~') ? replace_head(buf) : NULL;
+                buf[len] = NULL_CHAR;
+                char *arg = (buf[0] == TILDE) ? replace_head(buf) : NULL;
 
                 if (!arg) arg = strdup(buf);
                 command_out->argv[command_out->argc++] = arg;
@@ -41,11 +54,11 @@ bool parse(command_t *command_out, char *input) {
                 len = 0;
             }
 
-            if (*ch == '\0') break;
+            if (*ch == NULL_CHAR) break;
 
             continue;
         }
- 
+
         buf[len++] = *ch;
     }
 
